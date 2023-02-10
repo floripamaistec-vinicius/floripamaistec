9db3428a28a013dd673464d6e0c2c01386dfa287
floripamaistec-vinicius <vinicius_bernardes@estudante.sesisenai.org.br>
2023-02-10 15:37:06 -0300

    semana01exercicio01

---
+++ semana01/exercicio01/Program.cs
@@ -0,0 +1,18 @@
+﻿List<string> listaDePerguntas = new List<string>();
+List<string> listaDeRespostas = new List<string>();
+
+listaDePerguntas.Add("Nome: ");
+listaDePerguntas.Add("Sobrenome: ");
+listaDePerguntas.Add("Idade: ");
+listaDePerguntas.Add("Cor Favorita: ");
+
+for (int i = 0; i < listaDePerguntas.Count; i++)
+{
+    Console.WriteLine(listaDePerguntas[i]);
+    listaDeRespostas.Add(Console.ReadLine());
+}
+
+for (int i = 0; i < listaDeRespostas.Count; i++)
+{
+    Console.WriteLine(listaDePerguntas[i] + listaDeRespostas[i]);
+}
