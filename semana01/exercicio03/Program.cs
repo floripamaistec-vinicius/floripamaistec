750b7b74e75d684a5c72431acf2340119805026b
floripamaistec-vinicius <vinicius_bernardes@estudante.sesisenai.org.br>
2023-02-10 15:56:34 -0300

    semana01exercicio03

---
+++ semana01/exercicio03/Program.cs
@@ -0,0 +1,22 @@
+﻿List<float> listaDeNotas = new List<float>();
+
+for (int iteradorEntradaNaListaDeNotas = 0; iteradorEntradaNaListaDeNotas < 3; iteradorEntradaNaListaDeNotas++)
+{
+    Console.WriteLine($"Nota {iteradorEntradaNaListaDeNotas + 1}:");
+    listaDeNotas.Add(float.Parse(Console.ReadLine()));
+}
+
+float media = listaDeNotas.Sum() / 3;
+
+if (media >= 6)
+{
+    Console.WriteLine($"Aluno aprovado com média: {media}");
+}
+else if (media >= 5)
+{
+    Console.WriteLine($"Aluno em recuperação com média: {media}");
+}
+else
+{
+    Console.WriteLine($"Aluno reprovado com média: {media}");
+}
