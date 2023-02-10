ca719d3d01db3b14cfd2e7f3940545ae1f05edc3
floripamaistec-vinicius <vinicius_bernardes@estudante.sesisenai.org.br>
2023-02-10 16:39:48 -0300

    semana01exercicio06

---
+++ semana01/exercicio06/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Digite o número que será tabuado:");
+int numeroQueSeraTabuado = int.Parse(Console.ReadLine());
+
+for (int iterador = 0; iterador <= 10; iterador++)
+{
+    Console.WriteLine($"{numeroQueSeraTabuado} x {iterador} = {numeroQueSeraTabuado * iterador}");
+}
