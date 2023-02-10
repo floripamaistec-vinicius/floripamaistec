dce2142347cf7853189b27a2cfb89b81386aefcb
floripamaistec-vinicius <vinicius_bernardes@estudante.sesisenai.org.br>
2023-02-10 16:38:12 -0300

    semana01exercicio05

---
+++ semana01/exercicio05/Program.cs
@@ -0,0 +1,27 @@
+﻿List<float> ladosDoTriangulo = new List<float>();
+
+for (int iterador = 0; iterador < 3; iterador++)
+{
+    Console.WriteLine($"Valor do {iterador + 1}º lado:");
+    ladosDoTriangulo.Add(float.Parse(Console.ReadLine()));
+}
+
+if (ladosDoTriangulo[0] < (ladosDoTriangulo[1] + ladosDoTriangulo[2]) || ladosDoTriangulo[1] < (ladosDoTriangulo[0] + ladosDoTriangulo[2]) || ladosDoTriangulo[2] < (ladosDoTriangulo[0] + ladosDoTriangulo[2]))
+{
+    if (ladosDoTriangulo[0] == ladosDoTriangulo[1] && ladosDoTriangulo[0] == ladosDoTriangulo[2])
+    {
+        Console.WriteLine("Triângulo Equilátero");
+    }
+    else if (ladosDoTriangulo[0] != ladosDoTriangulo[1] && ladosDoTriangulo[0] != ladosDoTriangulo[2] && ladosDoTriangulo[1] != ladosDoTriangulo[2])
+    {
+        Console.WriteLine("Triângulo Escaleno");
+    }
+    else
+    {
+        Console.WriteLine("Triângulo Isóceles");
+    }
+}
+else
+{
+    Console.WriteLine("Não é um Triangulo.");
+}
