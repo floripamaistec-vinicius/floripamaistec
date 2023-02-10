1177b38b8465086c8e766ee6431e5a2185b9c58e
floripamaistec-vinicius <vinicius_bernardes@estudante.sesisenai.org.br>
2023-02-10 16:51:00 -0300

    semana01exercicio08

---
+++ semana01/exercicio08/Program.cs
@@ -0,0 +1,22 @@
+﻿static int CalculaSequenciaFibonacciRecursivamente(int enesimoTermo)
+{
+    if (enesimoTermo == 0 || enesimoTermo == 1)
+    {
+        return enesimoTermo;
+    }
+    else
+    {
+        return CalculaSequenciaFibonacciRecursivamente(enesimoTermo - 1) + CalculaSequenciaFibonacciRecursivamente(enesimoTermo - 2);
+    }
+}
+
+int informaValorDoParametroNoMetodo()
+{
+    Console.WriteLine("Calcular a sequência de Fibonacci até:");
+    return int.Parse(Console.ReadLine());
+}
+
+for (int iterador = informaValorDoParametroNoMetodo(); iterador > 0; iterador--)
+{
+    Console.WriteLine("{0}º Termo: {1}", iterador, CalculaSequenciaFibonacciRecursivamente(iterador - 1));
+}
