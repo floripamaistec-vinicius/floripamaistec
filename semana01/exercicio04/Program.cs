d3475be875e3f803ed201de6dcfcc63d69fd43fe
floripamaistec-vinicius <vinicius_bernardes@estudante.sesisenai.org.br>
2023-02-10 16:34:14 -0300

    semana01exercicio04

---
+++ semana01/exercicio04/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Salário bruto: ");
+float salarioBruto = float.Parse(Console.ReadLine());
+
+if (salarioBruto <= 900)
+{
+    Console.WriteLine("Imposto ZERO.");
+}
+else if (salarioBruto <= 1500)
+{
+    Console.WriteLine($"Salário: R$ {salarioBruto}. Imposto: R$ {salarioBruto * 0.05}");
+}
+else if (salarioBruto <= 2500)
+{
+    Console.WriteLine($"Salário: R$ {salarioBruto}. Imposto: R$ {salarioBruto * 0.1}");
+}
+else
+{
+    Console.WriteLine($"Salário: R$ {salarioBruto}. Imposto: R$ {salarioBruto * 0.2}");
+}
