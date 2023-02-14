4d581e666a737c61ee2eec5bb710ccb7f2f7443e
floripamaistec-vinicius <vinicius_bernardes@estudante.sesisenai.org.br>
2023-02-14 17:05:07 -0300

    semana02exercicio08

---
+++ semana02/exercicio08/Pessoa.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace exercicio08
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public int idade;
+        public Pessoa(string nome, DateTime dataNascimento)
+        {
+            Nome = nome;
+            DataNascimento = dataNascimento;
+        }
+        private void CalcularIdade()
+        {
+            var dataAtual = DateTime.Now;
+            idade = dataAtual.Year - DataNascimento.Year;
+            if (DataNascimento > dataAtual.AddYears(-idade))
+            {
+                idade--;
+            }
+        }
+        public void MostrarIdade()
+        {
+            CalcularIdade();
+
+            Console.WriteLine($"{Nome} tem a idade de {idade} anos.");
+        }
+    }
+}
---
+++ semana02/exercicio08/Program.cs
@@ -0,0 +1,5 @@
+﻿using exercicio08;
+
+DateTime DataNascimento = new DateTime(1988, 12, 28);
+Pessoa pessoa = new Pessoa("vinicius", DataNascimento);
+pessoa.MostrarIdade();
