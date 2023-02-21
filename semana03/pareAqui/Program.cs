de73ff3ea5c2b0d3510c038ca29c519437a07619
floripamaistec-vinicius <vinicius_bernardes@estudante.sesisenai.org.br>
2023-02-21 02:00:56 -0300

    commit

--- semana03/pareAqui/Program.cs
+++ semana03/pareAqui/Program.cs
@@ -1,5 +1,6 @@
 ﻿using pareAqui;
 using System.Globalization;
+using System.Net.Sockets;
 // Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
 Thread.CurrentThread.CurrentCulture = new CultureInfo("pt-BR");
 
@@ -17,6 +18,8 @@ do
     opcao = int.Parse(Console.ReadLine());
     if (opcao == 1) { CadastrarCarro(); }
     else if (opcao == 2) { MarcarEntrada(); }
+    else if (opcao == 3) { FecharTicket(); }
+    else if (opcao == 4) { Historico(); }
     else { Console.WriteLine("Opção inexistente. Digite novamente."); }
 } while (opcao != 5);
 
@@ -35,9 +38,27 @@ void CadastrarCarro() {
 
 void MarcarEntrada()
 {
+    bool entrada = true;
     Carro carroCadastrado = ObterCarro();
-    verificarTicketAtivo(carroCadastrado);
+    verificarTicketAtivo(carroCadastrado, entrada);
 }
+
+void FecharTicket()
+{
+    bool entrada = false;
+    Carro carroCadastrado = ObterCarro();
+    verificarTicketAtivo(carroCadastrado, entrada);
+}
+
+void Historico()
+{
+    Carro carroCadastrado = ObterCarro();
+    foreach (Ticket ticket in carroCadastrado.listTicket) {
+        Console.WriteLine(String.Format("Entrada: {0} --- Saída: {1} --- Valores: {2:C}", ticket.entrada, ticket.saida, Convert.ToInt32(ticket.CalcularValor())));
+    }
+
+}
+
 
 Carro ObterCarro()
 {
@@ -56,17 +77,49 @@ Carro ObterCarro()
     return null;
 }
 
-void verificarTicketAtivo(Carro carroCadastrado)
+void verificarTicketAtivo(Carro carroCadastrado, bool entrada)
 {
-    int ticketAtual = carroCadastrado.listTicket.Count - 1;
-    if (carroCadastrado.listTicket[ticketAtual].ativo == false)
+    foreach (Ticket ticket in carroCadastrado.listTicket)
     {
-        criarTicket(ticketAtual, carroCadastrado);
+        if (entrada == true)
+        {
+            if (ticket.ativo == false)
+            {
+                gerarTicket(carroCadastrado, entrada, ticket);
+            }
+            else
+            {
+                Console.WriteLine("O veículo com placa {0} já possui ticket ativo.", carroCadastrado.placa);
+            }
+        } else
+        {
+            if (ticket.ativo == false)
+            {
+                Console.WriteLine("O veículo com placa {0} não possui ticket ativo.", carroCadastrado.placa);
+            }
+            else
+            {
+                gerarTicket(carroCadastrado, entrada, ticket);
+            }
+        }
     }
 }
 
-void criarTicket(int ticketAtual, Carro carroCadastrado)
+void gerarTicket(Carro carroCadastrado, bool entrada, Ticket ticket)
 {
-    carroCadastrado.listTicket[ticketAtual].entrada = DateTime.Now;
-    carroCadastrado.listTicket[ticketAtual].ativo = true;
+    if (entrada == true)
+    {
+        Ticket cadastrarTicket = new Ticket();
+        cadastrarTicket.entrada = DateTime.Now;
+        cadastrarTicket.ativo = true;
+        carroCadastrado.listTicket.Add(ticket);
+        Console.WriteLine("Ticket {0}/{1} criado.", carroCadastrado.placa, carroCadastrado.listTicket.Count);
+    } else
+    {
+        ticket.saida = DateTime.Now;
+        carroCadastrado.listTicket.Add(ticket);
+        Console.WriteLine("Tempo no Estacionamento: {0}", ticket.CalcularTempo());
+        Console.WriteLine("Valor no Estacionamento: {0}", ticket.CalcularValor());
+    }
+    
 }
