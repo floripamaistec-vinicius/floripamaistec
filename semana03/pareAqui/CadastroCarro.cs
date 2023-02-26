4078ca3a007c7624b89ccad7d7a23417ff6b0a4c
floripamaistec-vinicius <vinicius_bernardes@estudante.sesisenai.org.br>
2023-02-26 16:15:37 -0300

    commit

---
+++ semana03/pareAqui/CadastroCarro.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pareAqui
+{
+    public class CadastroCarro
+    {
+        static List<Carro> ListaDeCarros = new List<Carro>();
+        public void CadastrarCarro()
+        {
+            string Placa = InformarPlaca();
+            for (int index = 0; index < ListaDeCarros.Count; index++)
+            {
+
+            }
+        }
+        private string InformarPlaca()
+        {
+            Console.WriteLine("Placa:");
+            return Console.ReadLine();
+        }
+    }
+}
